429fca0c566893a8f7996a7491ad823488492bf1
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2023-12-10 12:10:06 -0500

    Update 1.go

--- 2023/Day 10/1.go
+++ 2023/Day 10/1.go
@@ -34,55 +34,59 @@ func main() {
 	m := len(inp[0])
 	dist := make([][]int, n)
 	ans := 0
+	sx := 0
+	sy := 0
 	for i := range inp {
 		for j := range inp[0] {
 			if inp[i][j] == 'S' {
-				for x := 0; x < n; x++ {
-					dist[x] = make([]int, m)
-					for y := 0; y < m; y++ {
-						dist[x][y] = -1
-					}
-				}
-				q := [][]int{{i, j}}
-				dist[i][j] = 0
-				for len(q) != 0 {
-					x, y := q[0][0], q[0][1]
-					ans = max(ans, dist[x][y])
-					q = q[1:]
-					if inp[x][y] == '.' {
-						dist[x][y] = -1
-						continue
-					}
-					mx, my := make(map[string][]int), make(map[string][]int)
-					//north (x - 1)
-					for _, s := range []string{"L", "J", "|", "S"} {
-						mx[s] = append(mx[s], -1)
-					}
-					//south (x + 1)
-					for _, s := range []string{"F", "7", "|", "S"} {
-						mx[s] = append(mx[s], 1)
-					}
-					//east (y + 1)
-					for _, s := range []string{"L", "F", "-", "S"} {
-						my[s] = append(my[s], 1)
-					}
-					//west
-					for _, s := range []string{"J", "7", "-", "S"} {
-						my[s] = append(my[s], -1)
-					}
-					for _, dx := range mx[string(inp[x][y])] { //going north-south
-						if slices.Contains(mx[string(inp[x+dx][y])], -dx) && dist[x+dx][y] == -1 && inp[x+dx][y] != '.' {
-							q = append(q, []int{x + dx, y})
-							dist[x+dx][y] = dist[x][y] + 1
-						}
-					}
-					for _, dy := range my[string(inp[x][y])] {
-						if slices.Contains(my[string(inp[x][y+dy])], -dy) && dist[x][y+dy] == -1 && inp[x][y+dy] != '.' {
-							q = append(q, []int{x, y + dy})
-							dist[x][y+dy] = dist[x][y] + 1
-						}
-					}
-				}
+				sx = i
+				sy = j
+			}
+		}
+	}
+	for x := 0; x < n; x++ {
+		dist[x] = make([]int, m)
+		for y := 0; y < m; y++ {
+			dist[x][y] = -1
+		}
+	}
+	q := [][]int{{sx, sy}}
+	dist[sx][sy] = 0
+	for len(q) != 0 {
+		x, y := q[0][0], q[0][1]
+		ans = max(ans, dist[x][y])
+		q = q[1:]
+		if inp[x][y] == '.' {
+			dist[x][y] = -1
+			continue
+		}
+		mx, my := make(map[string][]int), make(map[string][]int)
+		//north (x - 1)
+		for _, s := range []string{"L", "J", "|", "S"} {
+			mx[s] = append(mx[s], -1)
+		}
+		//south (x + 1)
+		for _, s := range []string{"F", "7", "|", "S"} {
+			mx[s] = append(mx[s], 1)
+		}
+		//east (y + 1)
+		for _, s := range []string{"L", "F", "-", "S"} {
+			my[s] = append(my[s], 1)
+		}
+		//west
+		for _, s := range []string{"J", "7", "-", "S"} {
+			my[s] = append(my[s], -1)
+		}
+		for _, dx := range mx[string(inp[x][y])] { //going north-south
+			if slices.Contains(mx[string(inp[x+dx][y])], -dx) && dist[x+dx][y] == -1 && inp[x+dx][y] != '.' {
+				q = append(q, []int{x + dx, y})
+				dist[x+dx][y] = dist[x][y] + 1
+			}
+		}
+		for _, dy := range my[string(inp[x][y])] {
+			if slices.Contains(my[string(inp[x][y+dy])], -dy) && dist[x][y+dy] == -1 && inp[x][y+dy] != '.' {
+				q = append(q, []int{x, y + dy})
+				dist[x][y+dy] = dist[x][y] + 1
 			}
 		}
 	}

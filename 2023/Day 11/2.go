b98c492a17336304236fd5c64d7412307e4f1a9b
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2023-12-11 10:08:30 -0500

    Update 2.go

--- 2023/Day 11/2.go
+++ 2023/Day 11/2.go
@@ -4,14 +4,12 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"slices"
 	"strings"
 )
 
 func abs(x int) int {
-	if x < 0 {
-		return -x
-	}
-	return x
+	return max(x, -x)
 }
 func swap(arr []string) []string {
 	ans := make([]string, len(arr[0]))
@@ -36,30 +34,25 @@ func main() {
 			inp[len(inp)-1] += string(input[i])
 		}
 	}
-	part1 := func(repeat int) int {
+	part1 := func(repeat int, arr []string) int {
 		t := 2
 		for t != 0 {
 			t--
-			inp = swap(inp)
-			l := len(inp)
+			arr = swap(arr)
+			l := len(arr)
 			for i := l - 1; i >= 0; i-- {
-				cur := inp[i]
-				if strings.Index(cur, "#") <= -1 {
+				cur := arr[i]
+				if strings.Index(cur, "#") == -1 {
 					for x := 0; x < repeat-1; x++ {
-						if i != l-1 {
-							inp = append(inp[:i+1], inp[i:]...)
-							inp[i] = cur
-						} else {
-							inp = append(inp, inp[i])
-						}
+						arr = slices.Insert(arr, i, cur)
 					}
 				}
 			}
 		}
 		galaxies := [][]int{}
-		for i := range inp {
-			for j := range inp[i] {
-				if inp[i][j] == '#' {
+		for i := range arr {
+			for j := range arr[i] {
+				if arr[i][j] == '#' {
 					galaxies = append(galaxies, []int{i, j})
 				}
 			}
@@ -72,10 +65,6 @@ func main() {
 		}
 		return sum / 2
 	}
-	orig := inp
-	x1 := part1(2)
-	inp = orig
-	x2 := part1(3)
 	mult := 1000000
-	fmt.Print((mult-2)*(x2-x1) + x1)
+	fmt.Print((mult-2)*part1(3, inp) - (mult-3)*part1(2, inp))
 }
